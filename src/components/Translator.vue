1f4ec96353f415f59cd6c3207f7536d321869f7d
Murray Summers <murray@tito.io>
2024-01-22 23:02:33 +0000

    Add the translation.

---
+++ src/components/Translator.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    {{ translated }} <br />
+    {{ original }} <br />
+    <input type="text" v-model="original" @input="translate"/>
+
+    <button @click.prevent="copyToClipboard">
+      Copy
+    </button>
+  </div>
+</template>
+
+<script>
+
+  import pinyin from "pinyin";
+
+  export default {
+    data(){
+      return {
+        original: "",
+        translated: null
+      }
+    },
+
+    methods: {
+      translate(){
+        this.translated = pinyin(this.original).flat(3).join(" ");
+      },
+      copyToClipboard(){
+        this.$clipboard(`${this.translated}\n${this.original}`)
+      }
+    }
+  }
+</script>
